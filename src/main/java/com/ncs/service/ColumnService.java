06031213e749abfb6d71f758b5fee362f6654511
DESKTOP-912HOCH\Green_Computer <mansung93@hotmail.com>
2020-06-29 12:09:25 +0900

    update

---
+++ src/main/java/com/ncs/service/ColumnService.java
@@ -0,0 +1,25 @@
+package com.ncs.service;
+
+import java.util.List;
+
+import com.ncs.util.Criteria;
+import com.ncs.util.SearchCriteria;
+import com.ncs.vo.ColumnVO;
+
+public interface ColumnService {
+	
+	int searchRowCount(SearchCriteria cri);
+	List<ColumnVO> searchList(SearchCriteria cri);
+	
+	int totalRowCount();
+	int countUp(ColumnVO vo);
+	
+	List<ColumnVO> columnList();
+	
+	int insert(ColumnVO vo);
+	
+	int update(ColumnVO vo);
+	
+	int delete(ColumnVO vo);
+
+}
