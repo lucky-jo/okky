c7c52271ed72048a30e4d59132336c20d2c29b2b
ildang100 <last2599@gmail.com>
2020-07-05 02:26:23 +0900

    댓글 등록시, 게시글의 댓글 갯수 증가 기능
    댓글 등록시, 게시글의 댓글 갯수가 1 증가 하도록 작성.

--- src/main/java/com/ncs/service/QnaReplyServiceImpl.java
+++ src/main/java/com/ncs/service/QnaReplyServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ncs.mapper.QnaMapper;
 import com.ncs.mapper.QnaReplyMapper;
 import com.ncs.vo.ReplyVO;
 
@@ -13,9 +14,13 @@ public class QnaReplyServiceImpl implements QnaReplyService {
 	
 	@Autowired
 	QnaReplyMapper rmapper;
+	
+	@Autowired
+	QnaMapper mapper;
 
 	@Override
 	public int register(ReplyVO rvo) {
+		mapper.replycountUp(rvo.getSeq());
 		return rmapper.register(rvo);
 	}
 
