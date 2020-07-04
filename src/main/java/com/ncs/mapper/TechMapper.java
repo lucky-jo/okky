2acd6c64d7ae94d67ab9961a90b3fad3a8b8efde
ildang100 <last2599@gmail.com>
2020-07-04 23:52:27 +0900

    Tech 게시판 Mapper
    기존의 QnA 게시판 Mapper 를 복사하여, Tech 에 맞게 부분 수정

---
+++ src/main/java/com/ncs/mapper/TechMapper.java
@@ -0,0 +1,21 @@
+package com.ncs.mapper;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.ncs.util.SearchCriteria;
+import com.ncs.vo.TechVO;
+
+@Repository
+public interface TechMapper {
+	
+	List<TechVO> selectList();
+	int insert(TechVO vo);
+	TechVO selectOne(TechVO vo);
+	int update(TechVO vo);
+	int delete(TechVO vo);
+	int totalRowCount();
+	List<TechVO> searchList(SearchCriteria cri);
+	int searchRowCount(SearchCriteria cri);
+}
