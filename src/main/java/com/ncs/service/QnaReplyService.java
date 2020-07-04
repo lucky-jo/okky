d91113b714300ff778775183590dbb65b1708b88
ildang100 <last2599@gmail.com>
2020-07-04 21:52:26 +0900

    Qna 댓글 Serivce / ServiceImpl / Mapper

---
+++ src/main/java/com/ncs/service/QnaReplyService.java
@@ -0,0 +1,15 @@
+package com.ncs.service;
+
+import java.util.List;
+
+import com.ncs.vo.ReplyVO;
+
+public interface QnaReplyService {
+	
+	int register(ReplyVO rvo);
+	List<ReplyVO> selectlist(int seq);
+	int update(ReplyVO rvo);
+	int delete(ReplyVO rvo);
+	int replyCount();
+
+}
---
+++ src/main/java/com/ncs/service/QnaReplyServiceImpl.java
@@ -0,0 +1,44 @@
+package com.ncs.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ncs.mapper.QnaReplyMapper;
+import com.ncs.vo.ReplyVO;
+
+@Service
+public class QnaReplyServiceImpl implements QnaReplyService {
+	
+	@Autowired
+	QnaReplyMapper rmapper;
+
+	@Override
+	public int register(ReplyVO rvo) {
+		return rmapper.register(rvo);
+	}
+
+	@Override
+	public List<ReplyVO> selectlist(int seq) {
+		return rmapper.selectlist(seq);
+	}
+
+	@Override
+	public int update(ReplyVO rvo) {
+		return rmapper.update(rvo);
+	}
+
+	@Override
+	public int delete(ReplyVO rvo) {
+		return rmapper.delete(rvo);
+	}
+
+	@Override
+	public int replyCount() {
+		return rmapper.replyCount();
+	}
+	
+	
+
+}
