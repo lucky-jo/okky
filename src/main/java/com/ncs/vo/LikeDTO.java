db6e8793ea49b90d76652ad00ff4c4847c8b660d
ildang100 <last2599@gmail.com>
2020-07-05 10:44:47 +0900

    좋아요 이력 조회용 DTO
    좋아요 이력조회를 하기 위한 정보를 담아 mybatis 에 사용된다.

---
+++ src/main/java/com/ncs/vo/LikeDTO.java
@@ -0,0 +1,43 @@
+package com.ncs.vo;
+
+public class LikeDTO {
+	
+	private String likeid;
+	private String board;
+	private int seq;
+	private int liketype;
+	
+	public int getLiketype() {
+		return liketype;
+	}
+	public void setLiketype(int liketype) {
+		this.liketype = liketype;
+	}
+
+	public String getLikeid() {
+		return likeid;
+	}
+	public void setLikeid(String likeid) {
+		this.likeid = likeid;
+	}
+	public String getBoard() {
+		return board;
+	}
+	public void setBoard(String board) {
+		this.board = board;
+	}
+	public int getSeq() {
+		return seq;
+	}
+	public void setSeq(int seq) {
+		this.seq = seq;
+	}
+	
+	@Override
+	public String toString() {
+		return "LikeDTO [likeid=" + likeid + ", board=" + board + ", seq=" + seq + ", liketype=" + liketype + "]";
+	}
+	
+	
+
+}
