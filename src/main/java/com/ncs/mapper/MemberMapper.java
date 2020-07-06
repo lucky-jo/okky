053863c9c17e74ff8f2a9a51fcb340ec8fcf9be4
ildang100 <last2599@gmail.com>
2020-07-06 20:39:48 +0900

    MemberMapper
    회원 정보를 조회하는 read 와, 회원가입을 하는 register 추가. Mapper 의 경우, 하나의 userid 에, 여러가지 auth 객체를 가질 수 있기 때문에,  resultMap 설정을 하여, 매핑한다.

--- src/main/java/com/ncs/mapper/MemberMapper.java
+++ src/main/java/com/ncs/mapper/MemberMapper.java
@@ -1,5 +1,12 @@
 package com.ncs.mapper;
 
+import com.ncs.vo.MemberVO;
+
 public interface MemberMapper {
+    MemberVO read(String userid);
+
+    int auth(String userid);
+
+    int register(MemberVO vo);
 
 }
