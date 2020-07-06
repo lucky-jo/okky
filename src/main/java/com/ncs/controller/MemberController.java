3c6d5489144fa4c5d1920d0ff242b69b8ea5639b
ildang100 <last2599@gmail.com>
2020-07-06 18:21:01 +0900

    HomeController / 회원가입 폼 이동과 회원가입 요청 처리

--- src/main/java/com/ncs/controller/MemberController.java
+++ src/main/java/com/ncs/controller/MemberController.java
@@ -1,17 +1,50 @@
 package com.ncs.controller;
 
+import com.ncs.service.MemberService;
+import com.ncs.vo.MemberVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @RequestMapping(value = "/member")
 @Controller
 public class MemberController {
 
-    @RequestMapping(value = "/login")
-    public void login() {
+    @Autowired
+    MemberService memberService;
 
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @RequestMapping(value = "/customLogin",method = RequestMethod.GET)
+    public void getLogin() {
     }
+
+//    @RequestMapping(value = "/login", method = RequestMethod.POST)
+//    public String postLogin(MemberVO vo) {
+//        memberService.read(vo.getUserid());
+//
+//
+//        return "redirect:/index";
+//    }
+
+    @RequestMapping(value = "/register",method = RequestMethod.GET)
+    public void getRegister(){
+    }
+
+    @RequestMapping(value = "/register",method = RequestMethod.POST)
+    public String postRegister(MemberVO vo){
+        String[] image = {"1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg","9.jpg"};
+        vo.setImage(image[(int)(Math.random()*9)]);
+        System.out.println(vo.getImage());
+        vo.setUserpw(bCryptPasswordEncoder.encode(vo.getUserpw()));
+        memberService.register(vo);
+        return "redirect:/index";
+    }
+
+
 
 }
