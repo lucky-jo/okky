06d4da8992f82965fe1b4afdd364c32dc830dca1
ildang100 <last2599@gmail.com>
2020-07-06 23:51:10 +0900

    LikeController / security 제어
    Controller 에서는 메서드를 실행하기 전에 로그인한 사용자와 현재 파라미터로 전달되는 작성자가 일치하는지 체크한다. @PreAuthorize 의 경우 문자열로 표현식을 지정할 수 있는데 이때 컨트롤러에 전달되는 파라미터를 같이 사용할 수 있어 유용하다.

--- src/main/java/com/ncs/controller/LikeController.java
+++ src/main/java/com/ncs/controller/LikeController.java
@@ -1,12 +1,13 @@
 package com.ncs.controller;
 
-import com.ncs.service.LikeCountService;
-import com.ncs.vo.LikeDTO;
-import com.ncs.vo.QnaVO;
-import com.ncs.vo.ReplyLikeDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ncs.service.LikeCountService;
+import com.ncs.vo.LikeDTO;
+import com.ncs.vo.ReplyLikeDTO;
 
 @RequestMapping(value = "/like")
 @Controller
@@ -15,23 +16,28 @@ public class LikeController {
     @Autowired
     LikeCountService likeCountService;
 
+    @PreAuthorize("principal.username == #dto.likeid")
     @RequestMapping(value = "/like")
     public String like(LikeDTO dto) {
         likeCountService.likeRegister(dto);
         return "redirect:/"+dto.getBoard()+"/get?seq="+dto.getSeq();
     }
 
+    @PreAuthorize("principal.username == #dto.likerid")
     @RequestMapping(value = "/replylike")
     public String replylike(ReplyLikeDTO dto) {
         likeCountService.replyLikeRegister(dto);
         return "redirect:/" + dto.getParentBoard() + "/get?seq=" + dto.getParentSeq();
     }
 
+    @PreAuthorize("principal.username == #dto.likerid")
     @RequestMapping(value = "/replydelete")
     public String replydelete(ReplyLikeDTO dto){
         likeCountService.replyLikeDelete(dto);
         return "redirect:/" + dto.getParentBoard() + "/get?seq=" + dto.getParentSeq();
     }
+    
+    @PreAuthorize("principal.username == #dto.likeid")
     @RequestMapping(value = "/delete")
     public String delete(LikeDTO dto) {
         likeCountService.likeDelete(dto);
