7df1b2be8f6c82ac5cbb6eaec757418dd5f00988
ildang100 <last2599@gmail.com>
2020-07-07 18:01:52 +0900

    맴버 정보 조회 페이지  작성 중

--- src/main/java/com/ncs/controller/MemberController.java
+++ src/main/java/com/ncs/controller/MemberController.java
@@ -3,11 +3,12 @@ package com.ncs.controller;
 import com.ncs.service.MemberService;
 import com.ncs.vo.MemberVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+import org.springframework.web.servlet.ModelAndView;
 
 @RequestMapping(value = "/member")
 @Controller
@@ -44,6 +45,14 @@ public class MemberController {
         memberService.register(vo);
         return "redirect:/index";
     }
+
+    @PreAuthorize("isAuthenticated()")
+    @RequestMapping(value = "/info")
+    public ModelAndView info(ModelAndView mv){
+
+        return mv;
+    }
+
 
 
 
