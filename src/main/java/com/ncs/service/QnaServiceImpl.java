a87fc9749e63012c1a2ffcba96de9ffeb3f02ed0
ildang100 <last2599@gmail.com>
2020-07-08 01:07:49 +0900

    Qna 게시판 활동시 점수 증감 추가 / QnaService
    게시판에서 유저가 활동시 점수 증감 하도록 작성하고, 트랜잭션 처리.

--- src/main/java/com/ncs/service/QnaServiceImpl.java
+++ src/main/java/com/ncs/service/QnaServiceImpl.java
@@ -1,11 +1,12 @@
 package com.ncs.service;
 
 import com.ncs.mapper.LikeCountMapper;
+import com.ncs.mapper.MemberMapper;
 import com.ncs.mapper.QnaMapper;
 import com.ncs.util.SearchCriteria;
 import com.ncs.vo.GetCountDTO;
+import com.ncs.vo.MemberVO;
 import com.ncs.vo.QnaVO;
-import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,7 +18,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
-@Log4j
 @Service
 public class QnaServiceImpl implements QnaService{
 
@@ -27,12 +27,24 @@ public class QnaServiceImpl implements QnaService{
 	@Autowired
 	LikeCountMapper likeCountMapper;
 	
+	@Autowired
+	MemberMapper memberMapper;
+	
+	@Autowired
+	MemberVO memberVO;
+	
 	@Override
 	public List<QnaVO> selectList(){
 		return mapper.selectList();
 	}
+	
+	@Transactional
 	@Override
 	public int insert(QnaVO vo) {
+		HttpServletRequest request = ((ServletRequestAttributes)RequestContextHolder.currentRequestAttributes()).getRequest();
+		memberVO.setUserid(request.getRemoteUser());
+		memberVO.setApoint(30);
+		memberMapper.pointUp(memberVO);
 		return mapper.insert(vo);
 	}
 
@@ -47,12 +59,14 @@ public class QnaServiceImpl implements QnaService{
 			dto.setToday(getFolder());
 			dto.setSeq(vo.getSeq());
 			if (mapper.getcount(dto) == 0 ) {
-				log.info("조회한 적이 없네.");
 				mapper.registercount(dto);
 				mapper.countUp(vo.getSeq());
-				log.info("그럼 조회수 증가");
 			}
+			memberVO.setUserid(request.getRemoteUser());
+			memberVO.setApoint(1);
+			memberMapper.pointUp(memberVO);
 		}
+		
 
 		return mapper.selectOne(vo);
 	}
