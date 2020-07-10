e2d83154ec04f1cfb049f785bf2a2deccf9aed4b
ildang100 <last2599@gmail.com>
2020-07-10 18:03:02 +0900

    클린코드 / 객체명 수정
    여러 Service 를 사용하게 되면서, 객체명의 혼동이 생길수 있어. 더 명확한 객체명으로 변경.

--- src/main/java/com/ncs/controller/QnaReplyController.java
+++ src/main/java/com/ncs/controller/QnaReplyController.java
@@ -8,18 +8,19 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @RequestMapping("/qnareply")
 @Controller
 public class QnaReplyController {
 	
 	@Autowired
-	QnaReplyService rservice;
+	QnaReplyService qnaReplyService;
 
 	@PreAuthorize("isAuthenticated()")
 	@RequestMapping(value = "/register", method = RequestMethod.POST )
 	public String register(ReplyVO rvo) {
-		rservice.register(rvo);
+		qnaReplyService.register(rvo);
 		return ("redirect:/qna/get?seq=" + rvo.getSeq());
 	}
 
@@ -32,8 +33,15 @@ public class QnaReplyController {
 	@PreAuthorize("isAuthenticated()")
 	@RequestMapping(value = "/delete")
 	public String delete(ReplyVO rvo) {
-		rservice.delete(rvo);
+		qnaReplyService.delete(rvo);
 		return "redirect:/qna/get?seq=" + rvo.getSeq();
+	}
+
+	@RequestMapping(value = "/modify", method = RequestMethod.GET )
+	public ModelAndView getModigy(ModelAndView mv, ReplyVO replyVO) {
+		mv.addObject("get",qnaReplyService.get(replyVO));
+		mv.setViewName("jsonView");
+		return mv;
 	}
 
 
