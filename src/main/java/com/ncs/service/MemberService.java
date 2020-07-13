d06b36aafb026279744daa5011e2f6e1565793ce
ildang100 <last2599@gmail.com>
2020-07-13 18:16:54 +0900

    이메일 인증 서비스 추가. 수정 중

--- src/main/java/com/ncs/service/MemberService.java
+++ src/main/java/com/ncs/service/MemberService.java
@@ -1,16 +1,19 @@
 package com.ncs.service;
 
 import com.ncs.util.SearchCriteria;
+import com.ncs.vo.AuthKeyDTO;
 import com.ncs.vo.MemberVO;
 import com.ncs.vo.QnaVO;
 
+import javax.mail.MessagingException;
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 
 public interface MemberService {
 
     MemberVO read(String userid);
 
-    int register(MemberVO vo);
+    int register(MemberVO vo) throws MessagingException, UnsupportedEncodingException;
     
     MemberVO get(String userid);
 
@@ -23,4 +26,6 @@ public interface MemberService {
     List<Integer> historyRowCount(SearchCriteria cri);
 
     int useridDuplicate(MemberVO memberVO);
+
+    int getAuth(AuthKeyDTO authKeyDTO);
 }
--- src/main/java/com/ncs/service/MemberServiceImpl.java
+++ src/main/java/com/ncs/service/MemberServiceImpl.java
@@ -1,13 +1,19 @@
 package com.ncs.service;
 
 import com.ncs.mapper.MemberMapper;
+import com.ncs.util.MailHandler;
 import com.ncs.util.SearchCriteria;
+import com.ncs.util.TempKey;
+import com.ncs.vo.AuthKeyDTO;
 import com.ncs.vo.MemberVO;
 import com.ncs.vo.QnaVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.mail.MessagingException;
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 
 @Service
@@ -16,6 +22,9 @@ public class MemberServiceImpl implements MemberService {
     @Autowired
     MemberMapper memberMapper;
 
+    @Autowired
+    JavaMailSender mailSender;
+
     @Override
     public MemberVO read(String userid) {
         return memberMapper.read(userid);
@@ -23,7 +32,24 @@ public class MemberServiceImpl implements MemberService {
 
     @Transactional
     @Override
-    public int register(MemberVO memberVO) {
+    public int register(MemberVO memberVO) throws MessagingException, UnsupportedEncodingException {
+        AuthKeyDTO authKeyDTO = new AuthKeyDTO();
+        TempKey tempKey = new TempKey();
+        MailHandler mailHandler = new MailHandler(mailSender);
+        String authkey = tempKey.getKey(50,true);
+        authKeyDTO.setUserid(memberVO.getUserid());
+        authKeyDTO.setAuthkey(authkey);
+        mailHandler.setSubject("[홈페이지 이메일 인증");
+        mailHandler.setText(
+                "<h1>메일인증</h1>" +
+                "<a href='http://localhost/member/authkey?userid=" + memberVO.getUserid() +
+                "&authkey=" + authkey +
+                "' target='_blenk'>이메일 인증 확인</a>");
+        mailHandler.setFrom("last2599@gmail.com","Okkay");
+        mailHandler.setTo(memberVO.getEmail());
+        mailHandler.send();
+        memberMapper.addAuthKey(authKeyDTO);
+        memberMapper.addAuthKey(authKeyDTO);
         memberMapper.register(memberVO);
         return memberMapper.auth(memberVO.getUserid());
     }
@@ -56,6 +82,11 @@ public class MemberServiceImpl implements MemberService {
     @Override
     public int useridDuplicate(MemberVO memberVO) {
         return memberMapper.useridDuplicate(memberVO);
+    }
+
+    @Override
+    public int getAuth(AuthKeyDTO authKeyDTO) {
+        return memberMapper.getAuthKey(authKeyDTO);
     }
 
 }
