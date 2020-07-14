7563d2fa20d9440ef5cbd7c1743b5c6814ec664a
DESKTOP-912HOCH\Green_Computer <jslgrant913@gmail.com>
2020-07-14 17:13:16 +0900

    JobsVO에 board 삽입(게시판 위치 알림 기능)

--- src/main/java/com/ncs/vo/JobsVO.java
+++ src/main/java/com/ncs/vo/JobsVO.java
@@ -12,14 +12,10 @@ public class JobsVO {
 	private int exist;
 	private String category;
 	private String updatedate;
+	private int replycount;
+	private String board;
 	
 	
-	public String getCategory() {
-		return category;
-	}
-	public void setCategory(String category) {
-		this.category = category;
-	}
 	public String getId() {
 		return id;
 	}
@@ -44,25 +40,6 @@ public class JobsVO {
 	public void setRegdate(String regdate) {
 		this.regdate = regdate;
 	}
-
-	public int getExist() {
-		return exist;
-	}
-	public void setExist(int exist) {
-		this.exist = exist;
-	}
-	public String getUpdatedate() {
-		return updatedate;
-	}
-	public void setUpdatedate(String updatedate) {
-		this.updatedate = updatedate;
-	}
-	public int getLikecount() {
-		return likecount;
-	}
-	public void setLikecount(int likecount) {
-		this.likecount = likecount;
-	}
 	public int getSeq() {
 		return seq;
 	}
@@ -75,6 +52,45 @@ public class JobsVO {
 	public void setCnt(int cnt) {
 		this.cnt = cnt;
 	}
+	public int getLikecount() {
+		return likecount;
+	}
+	public void setLikecount(int likecount) {
+		this.likecount = likecount;
+	}
+	public int getExist() {
+		return exist;
+	}
+	public void setExist(int exist) {
+		this.exist = exist;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getUpdatedate() {
+		return updatedate;
+	}
+	public void setUpdatedate(String updatedate) {
+		this.updatedate = updatedate;
+	}
+	public int getReplycount() {
+		return replycount;
+	}
+	public void setReplycount(int replycount) {
+		this.replycount = replycount;
+	}
+	public String getBoard() {
+		return board;
+	}
+	public void setBoard(String board) {
+		this.board = board;
+	}
+	
+	
+	
 
 	
 
