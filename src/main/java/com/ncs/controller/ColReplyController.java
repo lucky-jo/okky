a8e1015683b2855e8bae73fa0b6dd61479a6803c
DESKTOP-912HOCH\Green_Computer <cdkim1226@gmail.com>
2020-07-14 16:53:34 +0900

    댓글 수정 컨트롤러 추가

--- src/main/java/com/ncs/controller/ColReplyController.java
+++ src/main/java/com/ncs/controller/ColReplyController.java
@@ -5,6 +5,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.ncs.service.ColReplyService;
 import com.ncs.vo.ColumnVO;
@@ -37,5 +38,17 @@ public class ColReplyController {
 		return "redirect:/columnm/get?seq=" + rvo.getSeq();
 	}
 
+	@PreAuthorize("principal.username == #replyVO.rid")
+	@RequestMapping(value = "/modify", method = RequestMethod.POST)
+	public String postModify(ReplyVO replyVO) {
+		colReplyService.modify(replyVO);
+		return "redirect:/columnm/get?seq=" + replyVO.getSeq();
+	}
 
+	@RequestMapping(value = "/modify", method = RequestMethod.GET )
+	public ModelAndView getModigy(ModelAndView mv, ReplyVO replyVO) {
+		mv.addObject("get",colReplyService.get(replyVO));
+		mv.setViewName("jsonView");
+		return mv;
+	}
 }
--- src/main/java/com/ncs/controller/ComReplyController.java
+++ src/main/java/com/ncs/controller/ComReplyController.java
@@ -1,11 +1,14 @@
 package com.ncs.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.ncs.service.ComReplyService;
+import com.ncs.vo.ComunityVO;
 import com.ncs.vo.ReplyVO;
 
 @RequestMapping("/comreply")
@@ -15,15 +18,38 @@ public class ComReplyController {
 	@Autowired
 	ComReplyService comReplyService;
 	
+	@PreAuthorize("isAuthenticated()")
 	@RequestMapping(value = "/register", method = RequestMethod.POST)
 	public String register(ReplyVO rvo) {
 		comReplyService.register(rvo);
 		return ("redirect:/comunity/get?seq="+rvo.getSeq());
 	}
 	
+	@PreAuthorize("isAuthenticated()")
+	@RequestMapping(value = "/register", method = RequestMethod.GET )
+	public String getRegister(ComunityVO comunityVO) {
+		return ("redirect:/comunity/get?seq=" + comunityVO.getSeq());
+	}
+	
+	@PreAuthorize("isAuthenticated()")
 	@RequestMapping(value = "/delete")
 	public String delete(ReplyVO rvo) {
 		comReplyService.delete(rvo);
 		return "redirect:/comunity/get?seq="+rvo.getSeq();
 	}
+	
+	@PreAuthorize("principal.username == #replyVO.rid")
+	@RequestMapping(value = "/modify", method = RequestMethod.POST)
+	public String postModify(ReplyVO replyVO) {
+		comReplyService.modify(replyVO);
+		return "redirect:/comunity/get?seq=" + replyVO.getSeq();
+	}
+
+	@RequestMapping(value = "/modify", method = RequestMethod.GET )
+	public ModelAndView getModigy(ModelAndView mv, ReplyVO replyVO) {
+		mv.addObject("get",comReplyService.get(replyVO));
+		mv.setViewName("jsonView");
+		return mv;
+	}
+	
 }
