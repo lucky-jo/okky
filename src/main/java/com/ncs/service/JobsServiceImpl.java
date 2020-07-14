2a77b9f2605b3952d341b57aca08c60d3a62d593
DESKTOP-912HOCH\Green_Computer <jslgrant913@gmail.com>
2020-07-14 17:12:43 +0900

    no message

--- src/main/java/com/ncs/service/JobsServiceImpl.java
+++ src/main/java/com/ncs/service/JobsServiceImpl.java
@@ -1,23 +1,31 @@
 package com.ncs.service;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ncs.mapper.JobsMapper;
+import com.ncs.mapper.LikeCountMapper;
 import com.ncs.util.SearchCriteria;
+import com.ncs.vo.GetCountDTO;
 import com.ncs.vo.JobsReplyVO;
 import com.ncs.vo.JobsVO;
 
 
 
 @Service
-public class JobsServiceImpl implements JobsService {
+public class JobsServiceImpl<GetCountDto> implements JobsService {
 	
 	
 	@Autowired
 	JobsMapper mapper;
+	
+	@Autowired
+	LikeCountMapper likeCountMapper;
+	
 	
 	@Override
 	public List<JobsVO>selectlist(){
@@ -29,7 +37,21 @@ public class JobsServiceImpl implements JobsService {
 	}
 	@Override
 	public JobsVO selectOne(JobsVO vo) {
+		GetCountDTO dto = new GetCountDTO();
+		dto.setId("Lee");
+		dto.setBoard(vo.getBoard());
+		dto.setToday(getFolder());
+		dto.setSeq(vo.getSeq());
+		if( mapper.getcount(dto) == 0) {
+			mapper.registercount(dto);
+			mapper.countUp(vo.getSeq());
+		}
 		return mapper.selectOne(vo);
+	}
+	private String getFolder() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		Date date = new Date();
+		return sdf.format(date);
 	}
 	@Override 
 	public int update(JobsVO vo) { 
@@ -47,7 +69,14 @@ public class JobsServiceImpl implements JobsService {
 	public List<JobsVO> searchList(SearchCriteria cri) { 
 		return mapper.searchList(cri); 
 	}
-	
+	@Override
+	public int totalRowCount() {
+		return mapper.totalRowCount();
+	}
+	@Override
+	public int countUp(JobsVO vo) {
+		return mapper.countUp(vo);
+	}
 	
 	
 	
