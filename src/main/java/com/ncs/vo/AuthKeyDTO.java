2d821f014ed1fe16d89756df0c97e53f713f182a
DESKTOP-912HOCH\Green_Computer <cdkim1226@gmail.com>
2020-07-15 18:03:20 +0900

    기존 DTO에서 새로 수정

--- src/main/java/com/ncs/vo/AuthKeyDTO.java
+++ src/main/java/com/ncs/vo/AuthKeyDTO.java
@@ -4,32 +4,41 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class AuthKeyDTO {
-    private String userid;
+    private String email;
     private String authkey;
+    @Override
+	public String toString() {
+		return "AuthKeyDTO [email=" + email + ", authkey=" + authkey + ", regdate=" + regdate + "]";
+	}
+
+	private String regdate;
 
     public AuthKeyDTO() {
 
     }
 
-    public String getAuthkey() {
-        return authkey;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public void setAuthkey(String authkey) {
-        this.authkey = authkey;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public String getUserid() {
-        return userid;
-    }
+	public String getAuthkey() {
+		return authkey;
+	}
 
-    public void setUserid(String userid) {
-        this.userid = userid;
-    }
+	public void setAuthkey(String authkey) {
+		this.authkey = authkey;
+	}
 
+	public String getRegdate() {
+		return regdate;
+	}
 
-    public AuthKeyDTO(String userid, String authkey ){
-        this.userid = userid;
-        this.authkey = authkey;
-    }
+	public void setRegdate(String regdate) {
+		this.regdate = regdate;
+	}
+
 }
