15e9349e3b66e7c3fe499372db5bbc2e67af6fae
DESKTOP-912HOCH\Green_Computer <jslgrant913@gmail.com>
2020-07-15 17:39:27 +0900

    no message

--- src/main/java/com/ncs/controller/JobsReplyController.java
+++ src/main/java/com/ncs/controller/JobsReplyController.java
@@ -17,19 +17,19 @@ public class JobsReplyController {
 	@Autowired
 	JobsReplyService jservice;
 	
-	@RequestMapping(value = "/rinsert", method = RequestMethod.POST)
+	@RequestMapping(value = "/rinsert", method = RequestMethod.POST)//댓글 입력
 	public ModelAndView rinsert(ModelAndView mv,JobsReplyVO rvo) {
 		if(jservice.rinsert(rvo)>0);
-		mv.addObject("message", "댓글달기완료");  
-		mv.setViewName("redirect:/jobs/detailForm?seq="+rvo.getSeq());
+		mv.addObject("get", "댓글달기완료");  
+		mv.setViewName("redirect:/jobs/get?seq="+rvo.getSeq());
 		return mv;
 	}
 	
-	@RequestMapping(value = "/jdelete")
+	@RequestMapping(value = "/jdelete")//댓글 삭제
 	public ModelAndView jdelete(ModelAndView mv,JobsReplyVO rvo) {
 		jservice.jdelete(rvo);
-		mv.addObject("message", "댓글삭제완료");
-		mv.setViewName("redirect:/jobs/detailForm?seq="+rvo.getSeq());
+		mv.addObject("get", "댓글삭제완료");
+		mv.setViewName("redirect:/jobs/get?seq="+rvo.getSeq());
 		return mv;
 	}
 }
