139d5ac92e98218be8925746d0bc7632f2d97292
DESKTOP-912HOCH\Green_Computer <cdkim1226@gmail.com>
2020-07-20 18:06:58 +0900

    찾는 ID를 이메일로 전송해주는 서비스 생성 임시 비밀번호를 이메일로 전송해주는 서비스 생성

--- src/main/java/com/ncs/service/MemberServiceImpl.java
+++ src/main/java/com/ncs/service/MemberServiceImpl.java
@@ -21,25 +21,24 @@ import com.ncs.vo.QnaVO;
 @Service
 public class MemberServiceImpl implements MemberService {
 
-    @Autowired
-    MemberMapper memberMapper;
+	@Autowired
+	MemberMapper memberMapper;
 
-    @Autowired
+	@Autowired
 	JavaMailSender mailSender;
-    
-    @Autowired
-    MailService mailService;
 
-    @Override
-    public MemberVO read(String userid) {
-        return memberMapper.read(userid);
-    }
+	@Autowired
+	MailService mailService;
 
-    @Transactional
-    @Override
-    public int register(MemberVO memberVO) throws MessagingException, UnsupportedEncodingException {
+	@Override
+	public MemberVO read(String userid) {
+		return memberMapper.read(userid);
+	}
 
-    			
+	@Transactional
+	@Override
+	public int register(MemberVO memberVO) throws MessagingException, UnsupportedEncodingException {
+
 //        AuthKeyDTO authKeyDTO = new AuthKeyDTO();
 //        TempKey tempKey = new TempKey();
 //        String authKey = tempKey.getKey(50, true);
@@ -50,24 +49,24 @@ public class MemberServiceImpl implements MemberService {
 //    			+ "<h3>" + memberVO.getUserid() + "님</h3><p>인증하기 버튼을 누르시면 로그인을 하실 수 있습니다 : "
 //    			+ "<a href='http://localhost:8080/member/authKey?userid="+ memberVO.getUserid() +"&authkey="+authKey+"'>인증하기</a></p>";
 //        mailService.authSendMail(memberVO.getEmail(), "Okky 커뮤니티에 가입하신걸 환영 합니다.", body);
-        memberMapper.register(memberVO);
-        return memberMapper.auth(memberVO.getUserid());
-    }
+		memberMapper.register(memberVO);
+		return memberMapper.auth(memberVO.getUserid());
+	}
 
 	@Override
 	public MemberVO get(String userid) {
 		return memberMapper.get(userid);
 	}
 
-    @Override
-    public int edit(MemberVO memberVO) {
-        return memberMapper.edit(memberVO);
-    }
+	@Override
+	public int edit(MemberVO memberVO) {
+		return memberMapper.edit(memberVO);
+	}
 
-    @Override
-    public int passwordChange(MemberVO memberVO) {
-        return memberMapper.passwordChange(memberVO);
-    }
+	@Override
+	public int passwordChange(MemberVO memberVO) {
+		return memberMapper.passwordChange(memberVO);
+	}
 
 	@Override
 	public List<QnaVO> historyBoardList(SearchCriteria cri) {
@@ -79,70 +78,127 @@ public class MemberServiceImpl implements MemberService {
 		return memberMapper.historyRowCount(cri);
 	}
 
-    @Override
-    public int useridDuplicate(MemberVO memberVO) {
-        return memberMapper.useridDuplicate(memberVO);
-    }
-
-    @Override
-    public int getAuth(AuthKeyDTO authKeyDTO) {
-        return memberMapper.getAuthKey(authKeyDTO);
-    }
-
-    @Override
-    public int nicknameDuplicate(MemberVO memberVO) {
-        return memberMapper.nicknameDuplicate(memberVO);
-    }
-
-	@Override 
-	public int emailDuplicate(MemberVO memberVO) { 
-		return memberMapper.emailDuplicate(memberVO); 
+	@Override
+	public int useridDuplicate(MemberVO memberVO) {
+		return memberMapper.useridDuplicate(memberVO);
 	}
-	
-    @Transactional
-    @Override
+
+	@Override
+	public int getAuth(AuthKeyDTO authKeyDTO) {
+		return memberMapper.getAuthKey(authKeyDTO);
+	}
+
+	@Override
+	public int nicknameDuplicate(MemberVO memberVO) {
+		return memberMapper.nicknameDuplicate(memberVO);
+	}
+
+	@Override
+	public int emailDuplicate(MemberVO memberVO) {
+		return memberMapper.emailDuplicate(memberVO);
+	}
+
+	@Transactional
+	@Override
 	public int sendAuthkey(AuthKeyDTO authdto) {
-    	// 1. db에 인증키 저장
-        Random r = new Random();
-        int dice = r.nextInt(8990)+1001 ; //이메일로 받는 인증코드 부분 (난수)
-        authdto.setAuthkey(dice);
-        int result = memberMapper.sendAuthkey(authdto);
-    	// 2. 인증키 전송
-    	if (result == 1) {
-        String to = authdto.getEmail();
-    	String subject = "회원가입 인증메일";
-    	String body =
-    				System.getProperty("line.separator")+ //한줄씩 줄간격을 두기위해 작성
-    	            
-    				System.getProperty("line.separator")+
-    	                    
-    	            "안녕하세요 회원님 저희 홈페이지를 찾아주셔서 감사합니다"
-    	            
-    	            +System.getProperty("line.separator")+
-    	            
-    	            System.getProperty("line.separator")+
-    	    
-    	            " 인증번호는 " +dice+ " 입니다. "
-    	            
-    	            +System.getProperty("line.separator")+
-    	            
-    	            System.getProperty("line.separator")+
-    	            
-    	            "받으신 인증번호를 홈페이지에 입력해 주시면 다음으로 넘어갑니다.";
-    	
-    	try {
-    		mailService.authSendMail(to, subject, body);
-		} catch (Exception e) {
-			System.out.println(e);
+		// 1. db에 인증키 저장
+		Random r = new Random();
+		int dice = r.nextInt(8990) + 1001; // 이메일로 받는 인증코드 부분 (난수)
+		authdto.setAuthkey(dice);
+		int result = memberMapper.sendAuthkey(authdto);
+		// 2. 인증키 전송
+		if (result == 1) {
+			String to = authdto.getEmail();
+			String subject = "회원가입 인증메일";
+			String body = System.getProperty("line.separator") + // 한줄씩 줄간격을 두기위해 작성
+
+					System.getProperty("line.separator") +
+
+					"안녕하세요 회원님 저희 홈페이지를 찾아주셔서 감사합니다"
+
+					+ System.getProperty("line.separator") +
+
+					System.getProperty("line.separator") +
+
+					" 인증번호는 " + dice + " 입니다. "
+
+					+ System.getProperty("line.separator") +
+
+					System.getProperty("line.separator") +
+
+					"받으신 인증번호를 홈페이지에 입력해 주시면 다음으로 넘어갑니다.";
+
+			try {
+				mailService.authSendMail(to, subject, body);
+			} catch (Exception e) {
+				System.out.println(e);
+			}
 		}
-    	}
 		return result;
 	}
 
 	@Override
 	public int authKeyCheck(AuthKeyDTO authdto) {
-		
+
 		return memberMapper.authKeyCheck(authdto);
 	}
-    
+
+	@Override
+	public int sendFindId(MemberVO membervo) {
+		membervo = memberMapper.sendFindId(membervo);
+		if (membervo != null) {
+			String to = membervo.getEmail();
+			String subject = "문의하신 계정 정보입니다";
+			String body = System.getProperty("line.separator") + // 한줄씩 줄간격을 두기위해 작성
+
+					System.getProperty("line.separator") +
+
+					"계정찾기"
+
+					+ System.getProperty("line.separator") +
+
+					System.getProperty("line.separator") +
+
+					"아이디 : " + membervo.getUserid();
+			try {
+				mailService.authSendMail(to, subject, body);
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+			return 1;
+		}
+		return 0;
+
+	}
+	
+	@Override
+	public int sendFindPassword(MemberVO membervo) {
+		Random r = new Random();
+		String dice = Integer.toString(r.nextInt(8990) + 1001); // 이메일로 받는 인증코드 부분 (난수)
+		membervo.setUserpw(dice);
+		membervo = memberMapper.sendFindPassword(membervo);
+		if(membervo != null) {
+			String to = membervo.getEmail();
+			String subject = "문의하신 계정 정보입니다";
+			String body = System.getProperty("line.separator") + // 한줄씩 줄간격을 두기위해 작성
+
+					System.getProperty("line.separator") +
+
+					"계정찾기"
+
+					+ System.getProperty("line.separator") +
+
+					System.getProperty("line.separator") +
+
+					"임시 비밀번호 : " + dice;
+			try {
+				mailService.authSendMail(to, subject, body);
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+			return 1;
+		}
+		return 0;
+	}
+	
 }
